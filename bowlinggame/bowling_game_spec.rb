559f585e1de715885268b3cb8b547efa42a774ca
Wouter Lagerweij <wouter@lagerweij.com>
2016-02-08 20:51:05 +0100

    Added empty classes to get started with

---
+++ bowlinggame/bowling_game_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative 'bowling_game'
+
+describe 'Bowling Game tests' do
+
+  it 'should create a new class' do
+    @game = BowlingGame.new
+  end
+
+end
---
+++ fizzbuzz/fizz_buzz_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative 'fizz_buzz'
+
+describe 'Fizz Buzz tests' do
+
+  it 'should create a new class' do
+    fizzbuzz = FizzBuzz.new
+  end
+
+end
