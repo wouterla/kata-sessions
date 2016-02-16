93025c782a889f65576dd40f4f604af2f89ad516
jvanderploeg <jvanderploeg@schubergphilis.com>
2016-02-16 10:52:40 +0100

    initial commit

--- mars-rover/mars_rover.rb
+++ mars-rover/mars_rover.rb
@@ -1,3 +1,79 @@
 class MarsRover
+  def initialize(x,y,facing)
+    @x = x
+    @y = y
+    @facing = facing
+  end
 
+  def position
+    return [@x,@y,@facing]
+  end
+
+  def forward
+    case @facing
+    when :north
+      @y += 1
+    when :south
+      @y -= 1
+    when :west
+      @x -= 1
+    when :east
+      @x += 1
+    end
+  end
+
+  def backward
+    case @facing
+    when :north
+      @y -= 1
+    when :south
+      @y += 1
+    when :west
+      @x += 1
+    when :east
+      @x -= 1
+    end
+  end
+
+  def left
+    case @facing
+    when :north
+      @facing = :west
+    when :south
+      @facing = :east
+    when :west
+      @facing = :south
+    when :east
+      @facing = :north
+    end
+  end
+
+  def right
+    case @facing
+    when :north
+      @facing = :east
+    when :south
+      @facing = :west
+    when :west
+      @facing = :north
+    when :east
+      @facing = :south
+    end
+  end
+
+  def move(move_string)
+    move_string.split(//).each do |move|
+      case move
+      when 'f'
+        forward
+      when 'b'
+        backward
+      when 'l'
+        left
+      when 'r'
+        right
+      end
+      
+    end
+  end
 end
--- mars-rover/mars_rover_spec.rb
+++ mars-rover/mars_rover_spec.rb
@@ -3,7 +3,80 @@ require_relative 'mars_rover'
 
 describe 'A Mars Rover' do
 
-  it 'should create a new rover' do
-    rover = MarsRover.new
+  it 'will return the start position and facing' do
+    rover = MarsRover.new(0,0,:north)
+    expect(rover.position).to match_array([0,0,:north])
   end
+
+it 'will saves its current position' do
+  rover = MarsRover.new(2,1,:south)
+  expect(rover.position).to match_array([2,1,:south])
+end
+
+  it 'will do one forward' do
+    rover = MarsRover.new(0,0,:north)
+    rover.forward
+    expect(rover.position).to match_array([0,1,:north])
+  end
+  it 'will do one backward' do
+    rover = MarsRover.new(0,1,:north)
+    rover.backward
+    expect(rover.position).to match_array([0,0,:north])
+  end
+  it 'will turn left' do
+    rover = MarsRover.new(0,1,:north)
+    rover.left
+    expect(rover.position).to match_array([0,1,:west])
+  end
+
+  it 'will turn left twice' do
+    rover = MarsRover.new(0,0,:north)
+    rover.left
+    rover.left
+    expect(rover.position).to match_array([0,0,:south])
+  end
+
+  it 'will turn right' do
+    rover = MarsRover.new(0,1,:north)
+    rover.right
+    expect(rover.position).to match_array([0,1,:east])
+  end
+
+  it 'will turn left and forward' do
+    rover = MarsRover.new(2,2,:north)
+    rover.left
+    rover.forward
+    expect(rover.position).to match_array([1,2,:west])
+  end
+
+  it 'will turn left and go backward' do
+    rover = MarsRover.new(2,2,:north)
+    rover.left
+    rover.backward
+    expect(rover.position).to match_array([3,2,:west])
+  end
+
+    it 'will turn right and go backward' do
+      rover = MarsRover.new(2,2,:north)
+      rover.right
+      rover.backward
+      expect(rover.position).to match_array([1,2,:east])
+    end
+
+    it 'will complete the exercise' do
+      rover = MarsRover.new(0,0,:north)
+      rover.forward
+      rover.forward
+      rover.right
+      rover.forward
+      rover.forward
+      expect(rover.position).to match_array([2,2,:east])
+    end
+
+    it 'takes a string of move commands' do
+      rover = MarsRover.new(0,0,:north)
+      rover.move('ffrff')
+      expect(rover.position).to match_array([2,2,:east])
+    end
+
 end
