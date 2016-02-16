5e50e4e0e6bb6cfe6ddcf0b6091a23277ae07a6e
David Bruce <dbruce@schubergphilis.com>
2016-02-16 23:05:27 +0100

    prepare to introduce a bounded grid

--- mars-rover/spec/mars_rover_spec.rb
+++ mars-rover/spec/mars_rover_spec.rb
@@ -3,78 +3,80 @@ require 'facing'
 require 'position'
 
 RSpec.describe 'A Mars Rover' do
-  before :each do
-    starting_position = Position.new(3, 3)
-    starting_facing = Facing.new(:north)
-    @rover = MarsRover.new(starting_position, starting_facing)
-  end
+  context 'when driving over an infinite plane' do
+    before :each do
+      starting_position = Position.new(3, 3)
+      starting_facing = Facing.new(:north)
+      @rover = MarsRover.new(starting_position, starting_facing)
+    end
 
-  let(:rover) { @rover }
+    let(:rover) { @rover }
 
-  it 'has a current position' do
-    expect(rover.position).to match_array([3, 3, :north])
-  end
+    it 'has a current position' do
+      expect(rover.position).to match_array([3, 3, :north])
+    end
 
-  it 'can move one forward' do
-    rover.forward
-    expect(rover.position).to match_array([3, 4, :north])
-  end
+    it 'can move one forward' do
+      rover.forward
+      expect(rover.position).to match_array([3, 4, :north])
+    end
 
-  it 'will do one backward' do
-    rover.backward
-    expect(rover.position).to match_array([3, 2, :north])
-  end
+    it 'will do one backward' do
+      rover.backward
+      expect(rover.position).to match_array([3, 2, :north])
+    end
 
-  it 'will turn left' do
-    rover.left
-    expect(rover.position).to match_array([3, 3, :west])
-  end
+    it 'will turn left' do
+      rover.left
+      expect(rover.position).to match_array([3, 3, :west])
+    end
 
-  it 'will turn left twice' do
-    rover.left
-    rover.left
-    expect(rover.position).to match_array([3, 3, :south])
-  end
+    it 'will turn left twice' do
+      rover.left
+      rover.left
+      expect(rover.position).to match_array([3, 3, :south])
+    end
 
-  it 'will turn right' do
-    rover.right
-    expect(rover.position).to match_array([3, 3, :east])
-  end
+    it 'will turn right' do
+      rover.right
+      expect(rover.position).to match_array([3, 3, :east])
+    end
 
-  it 'will turn left and forward' do
-    rover.left
-    rover.forward
-    expect(rover.position).to match_array([2, 3, :west])
-  end
+    it 'will turn left and forward' do
+      rover.left
+      rover.forward
+      expect(rover.position).to match_array([2, 3, :west])
+    end
 
-  it 'will turn left and go backward' do
-    rover.left
-    rover.backward
-    expect(rover.position).to match_array([4, 3, :west])
-  end
+    it 'will turn left and go backward' do
+      rover.left
+      rover.backward
+      expect(rover.position).to match_array([4, 3, :west])
+    end
 
-  it 'will turn right and go backward' do
-    rover.right
-    rover.backward
-    expect(rover.position).to match_array([2, 3, :east])
-  end
+    it 'will turn right and go backward' do
+      rover.right
+      rover.backward
+      expect(rover.position).to match_array([2, 3, :east])
+    end
 
-  it 'will complete the exercise' do
-    rover.forward
-    rover.forward
-    rover.right
-    rover.forward
-    rover.forward
-    expect(rover.position).to match_array([5, 5, :east])
-  end
+    it 'will complete the exercise' do
+      rover.forward
+      rover.forward
+      rover.right
+      rover.forward
+      rover.forward
+      expect(rover.position).to match_array([5, 5, :east])
+    end
 
-  it 'takes a string of move commands' do
-    rover.move('ffrff')
-    expect(rover.position).to match_array([5, 5, :east])
-  end
+    it 'takes a string of move commands' do
+      rover.move('ffrff')
+      expect(rover.position).to match_array([5, 5, :east])
+    end
 
-  it 'takes uppercase movement commands' do
-    rover.move('FFRFF')
-    expect(rover.position).to match_array([5, 5, :east])
+    it 'takes uppercase movement commands' do
+      rover.move('FFRFF')
+      expect(rover.position).to match_array([5, 5, :east])
+    end
   end
 end
