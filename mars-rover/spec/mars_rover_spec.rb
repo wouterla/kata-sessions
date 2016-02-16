afceb7361e9ef3a8d4c6a02c85ce47e3cbce3582
David Bruce <dbruce@schubergphilis.com>
2016-02-16 17:17:19 +0100

    reformat whitespace

--- mars-rover/spec/mars_rover_spec.rb
+++ mars-rover/spec/mars_rover_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'A Mars Rover' do
     expect(rover.position).to match_array([0,0,:north])
   end
 
-it 'will saves its current position' do
-  rover = MarsRover.new(2,1,:south)
-  expect(rover.position).to match_array([2,1,:south])
-end
+  it 'will saves its current position' do
+    rover = MarsRover.new(2,1,:south)
+    expect(rover.position).to match_array([2,1,:south])
+  end
 
   it 'will do one forward' do
     rover = MarsRover.new(0,0,:north)
@@ -55,27 +55,27 @@ end
     expect(rover.position).to match_array([3,2,:west])
   end
 
-    it 'will turn right and go backward' do
-      rover = MarsRover.new(2,2,:north)
-      rover.right
-      rover.backward
-      expect(rover.position).to match_array([1,2,:east])
-    end
+  it 'will turn right and go backward' do
+    rover = MarsRover.new(2,2,:north)
+    rover.right
+    rover.backward
+    expect(rover.position).to match_array([1,2,:east])
+  end
 
-    it 'will complete the exercise' do
-      rover = MarsRover.new(0,0,:north)
-      rover.forward
-      rover.forward
-      rover.right
-      rover.forward
-      rover.forward
-      expect(rover.position).to match_array([2,2,:east])
-    end
+  it 'will complete the exercise' do
+    rover = MarsRover.new(0,0,:north)
+    rover.forward
+    rover.forward
+    rover.right
+    rover.forward
+    rover.forward
+    expect(rover.position).to match_array([2,2,:east])
+  end
 
-    it 'takes a string of move commands' do
-      rover = MarsRover.new(0,0,:north)
-      rover.move('ffrff')
-      expect(rover.position).to match_array([2,2,:east])
-    end
+  it 'takes a string of move commands' do
+    rover = MarsRover.new(0,0,:north)
+    rover.move('ffrff')
+    expect(rover.position).to match_array([2,2,:east])
+  end
 
 end
