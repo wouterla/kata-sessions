c45617181373293a545db4ed04b48a2ce4f5a167
David Bruce <dbruce@schubergphilis.com>
2016-03-01 12:00:07 +0100

    at coffee break

--- gildedrose/lib/gilded_rose.rb
+++ gildedrose/lib/gilded_rose.rb
@@ -3,52 +3,68 @@ class GildedRose
   def initialize(items)
     @items = items
   end
+def not_Aged_Brie_and_not_Backstage_Passes(item)
+
+     if item.quality > 0
+        if item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = item.quality - 1
+        end
+      end
+
+end
+
+def aged_brie_or_backstage_passes(item)
+  if item.quality < 50
+    item.quality = item.quality + 1
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.sell_in < 11
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+      if item.sell_in < 6
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+  end
+end
+
+  def update_individual_item_quality(item)
+
+    if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
+      aged_brie_or_backstage_passes(item)
+    else
+      not_Aged_Brie_and_not_Backstage_Passes(item)
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in = item.sell_in - 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            if item.quality > 0
+              item.quality = item.quality - 1
+            end
+          end
+        else
+          item.quality = 0
+        end
+      end
+      
+      if item.name == "Aged Brie"  
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+  end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_individual_item_quality(item)
     end
   end
 end
--- gildedrose/spec/gilded_rose_spec.rb
+++ gildedrose/spec/gilded_rose_spec.rb
@@ -1,14 +1,78 @@
 require 'rspec'
+require 'spec_helper'
 require 'gilded_rose'
 
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
+    it 'an item "foo" can be updated' do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq("fixme")
+      expect(items[0].name).to eq("foo")
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq -1
     end
+
+    it 'quality degrades twice as fast for a normal item past its sell by date' do
+      items = [Item.new("foo", -5, 20)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq("foo")
+      expect(items[0].quality).to eq 18
+      expect(items[0].sell_in).to eq -6
+    end
+
+    it 'Aged Brie should increase in quality with age' do
+      items = [Item.new('Aged Brie', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq("Aged Brie")
+      expect(items[0].quality).to eq 2
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it 'Very Aged Brie should not increase in quality over 50' do
+      items = [Item.new('Aged Brie', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq("Aged Brie")
+      expect(items[0].quality).to eq 50
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it 'just Aged Brie should increase in quality' do
+      items = [Item.new('Aged Brie', 0, 48)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq("Aged Brie")
+      expect(items[0].quality).to eq 50
+      expect(items[0].sell_in).to eq -1
+    end
+
+    it 'Sulfuras should ont change in quality and sellin' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq("Sulfuras, Hand of Ragnaros")
+      expect(items[0].quality).to eq 10
+      expect(items[0].sell_in).to eq 0
+    end
+
+    it 'Backstage passes should be increase in quality in 2 when 10 days left' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 0)] 
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 2 
+      expect(items[0].sell_in).to eq 9
+    end
+    it 'Backstage passes should be increase in quality in 3 when 5 days left' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 3)] 
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 6 
+      expect(items[0].sell_in).to eq 4
+    end
+    it 'Backstage passes should be increase in quality in 0 when 1 day after concert' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, -1)] 
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0 
+      expect(items[0].sell_in).to eq -1
+    end
+
+
   end
 
 end
